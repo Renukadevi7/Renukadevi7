6d16580c86c8c7f62bc330560f492b2f495a8c3e
Renukadevi7 <95309928+Renukadevi7@users.noreply.github.com>
2021-11-30 14:22:29 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Java World");
+        System.out.println("!!!!!!!!!!!!!!");
+    }
+}
